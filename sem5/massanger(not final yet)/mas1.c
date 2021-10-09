e6978e66ba97e30b33a8a2db9e54704265180abf
Tameratlant <80047396+Tameratlant@users.noreply.github.com>
2021-10-09 22:00:54 +0300

    Update mas1.c

--- sem5/massanger(not final yet)/mas1.c
+++ sem5/massanger(not final yet)/mas1.c
@@ -6,58 +6,55 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-//int main(int argc, char** argv) {
-int main(){
-    char name1[] = "fifo1.fifo";
-    char name2[] = "fifo2.fifo";
-    
-    /*if(strcmp(argv[1], "1")
-        int fifo(*name1, *name2);
-    if(strcmp(argv[1], "2")
-        int fifo(*name2, *name1);
-    */
+int main() {
+
     int fd, result;
     size_t size;
     char string[100];
     char massage[100];
+    char name1[] = "fifo1.fifo";
+    char name2[] = "fifo2.fifo";
     /*
     (void)umask(0);
     if (mknod(name1, S_IFIFO | 0666, 0) < 0) {
         printf("Can\'t create FIFO 1\n");
         exit(-1);
     }
-    if (mknod(name1, S_IFIFO | 0666, 0) < 0) {
+    if (mknod(name2, S_IFIFO | 0666, 0) < 0) {
         printf("Can't\'t create FIFO 2\n");
         exit(-1);
     }
     */
-
+	result = fork();
     ///////////////////////////////////////////////
-    if ((result = fork()) > 0) {            //сидим в пишущем в консоль процессе
+    if (result > 0) {            //сидим в пишущем в консоль процессе
             if ((fd = open(name1, O_WRONLY)) < 0) {
                 printf("Can\'t open FIFO for writting\n");
                 exit(-1);
-            }
-            while (1) {
-                fgets(massage, 100, stdin);
+	    }
+             while (1) {
+            fgets(massage, 100, stdin);
 
-                write(fd, massage, 100);                                        //надеюсь, написали в FIFO
-            }
+            write(fd, massage, 100);	    //надеюсь, написали в FIFO
+	     }
             close(fd);
-        
+	     
     }
     ///////////////////////////////////////////////
 
     if (result == 0) {                                                       //сидим в читающем из FIFO процессе
         if ((fd = open(name2, O_RDONLY)) < 0) {
-                printf("Can\'t open FIFO for reading\n");
+            
+                printf("Can\'t open FIFO for writting\n");
                 exit(-1);
-            }
-        while (1) {
+	} 
+	     while (1) {
             read(fd, massage, 100);
-            printf("%s\n", massage);
-        }
-            close(fd);
+            
+	    printf("%s\n", massage);
+	     }
+	    close(fd);
+        
     }
     return 0;
 }
