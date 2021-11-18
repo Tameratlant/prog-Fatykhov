d631c68b32dfe8b64bb1a454bbe1a6f2b032600e
Tamerlan <fatykhov.tf@phystech.edu>
2021-11-19 00:43:48 +0300

    Almost fixed Server.c

--- sem 9/Server.c
+++ sem 9/Server.c
@@ -1,32 +1,49 @@
-
-
-/* Программа 2 для иллюстрации работы с очередями сообщений */
-
- 
-
-/* Эта программа получает доступ к очереди сообщений,
-и читает из нее сообщения с любым типом в порядке FIFO до тех пор,
-пока не получит сообщение с типом 255, которое будет служить
-сигналом прекращения работы. */
-
 #include <sys/types.h>
 #include <sys/ipc.h>
-#include <sys/msg.h>
-#include <string.h>
-#include <stdlib.h>
+#include <sys/sem.h>
 #include <stdio.h>
-#include <assert.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <math.h>
 
-#define LAST_MESSAGE 255 /* Тип сообщения для прекращения работы */
+int semid; /* IPC дескриптор для массива IPC семафоров */
+
+
+
+
+void mysemop(int a) {
+    struct sembuf mybuf;
+	mybuf.sem_op = a;
+	mybuf.sem_flg = 0;
+    mybuf.sem_num = 0;
+	if (semop(semid , &mybuf , 1) < 0) {
+		printf("Can\'t wait for condition\n");
+        exit(-1);
+	}
+}
+
+
 
 int main()
     {
+
+
+    
+
     int msqid; /* IPC дескриптор для очереди сообщений */
 
     char pathname[] = "Server.c"; /* Имя файла, использующееся для генерации ключа.
     Файл с таким именем должен существовать в текущей директории */
 
     key_t key; /* IPC ключ */
+
+    if((semid = semget(key, 2, 0666 | IPC_CREAT)) < 0){
+        printf("Can\'t get semid\n");
+        exit(-1);
+    }
 
     int len, maxlen; /* Реальная длина и максимальная длина информативной части сообщения */
 
@@ -45,12 +62,12 @@ int main()
         struct Info info;
         }mybuf;
 
-    /* Генерируем IPC ключ из имени файла 09-1a.c в текущей директории
+    /* Генерируем IPC ключ из имени файла Server.c в текущей директории
     и номера экземпляра очереди сообщений 0 */
 
     if((key = ftok(pathname,0)) < 0){
-    printf("Can\'t generate key\n");
-    exit(-1);
+        printf("Can\'t generate key\n");
+        exit(-1);
     }
 
     /* Пытаемся получить доступ по ключу к очереди сообщений, если она существует,
@@ -58,43 +75,45 @@ int main()
     read & write для всех пользователей */
 
     if((msqid = msgget(key, 0666 | IPC_CREAT)) < 0){
-    printf("Can\'t get msqid\n");
-    exit(-1);
+        printf("Can\'t get msqid\n");
+        exit(-1);
     }
-
+    //maxlen = sizeof(struct mymsgbuf);
+    maxlen = 300;
     while(1){
 
-    /* В бесконечном цикле принимаем сообщения любого типа в порядке FIFO с максимальной длиной информативной части 81 символ до тех пор, пока не поступит сообщение с типом LAST_MESSAGE*/
+        // В бесконечном цикле принимаем сообщения 
 
-    maxlen = 300;
+        if(( len = msgrcv(msqid, (struct msgbuf *) &mybuf, maxlen, 1, 0)) < 0){
+                printf("88 Can\'t receive message from queue\n");
+                exit(-1);
+            }
+        //Отправлем это же сообщение в процесс обработчик(такие сообщения будут иметь тип 2)
+        mybuf.mtype = 2;
+        len = sizeof(struct Info);
+        if (msgsnd(msqid, (struct msgbuf *) &mybuf, len, 0) < 0){
+            printf("Can\'t send message to queue\n");
+            msgctl(msqid, IPC_RMID, (struct msqid_ds *) NULL);
+            exit(-1);
+            }
+        int result = fork();
+        if (result > 0) {
 
-    if(( len = msgrcv(msqid, (struct msgbuf *) &mybuf, maxlen, 0, 0)) < 0){
-    printf("Can\'t receive message from queue\n");
-    exit(-1);
-    }
-    mybuf.mtype = mybuf.info.id;
-    mybuf.info.c = mybuf.info.a * mybuf.info.b;
+            if(( len = msgrcv(msqid, (struct msgbuf *) &mybuf, maxlen, 2, 0)) < 0){
+                printf("103 Can\'t receive message from queue\n");
+                exit(-1);
+            }
 
-    len = sizeof(mybuf) + 1;
-
-    /* Если принятое сообщение имеет тип LAST_MESSAGE, прекращаем работу и удаляем очередь сообщений из системы.
-    В противном случае печатаем текст принятого сообщения. */
-
-    if (msgsnd(msqid, (struct msgbuf *) &mybuf, len, 0) < 0){
-        printf("Can\'t send message to queue\n");
-        msgctl(msqid, IPC_RMID, (struct msqid_ds *) NULL);
-        exit(-1);
+        //И вот уже тут отсылаем сообщение обратно клиенту
+        mybuf.mtype = mybuf.info.id;
+        mybuf.info.c = mybuf.info.a * mybuf.info.b;
+        if (msgsnd(msqid, (struct msgbuf *) &mybuf, len, 0) < 0){
+            printf("Can\'t send message to queue\n");
+            msgctl(msqid, IPC_RMID, (struct msqid_ds *) NULL);
+            exit(-1);
+            }
         }
-
-
-    //if (mybuf.mtype == LAST_MESSAGE){
-    //msgctl(msqid, IPC_RMID, (struct msqid_ds *) NULL);
-    //exit(0);
-    
-
-    
     }
-    //msgctl(msqid, IPC_RMID, (struct msqid_ds *) NULL);
 
     return 0; /* Исключительно для отсутствия warning'ов при компиляции. */
 } 
